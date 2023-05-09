e0dd72604a88a65d5efc52ede3511368260be427
Gabriel Coutinho <gabriel.coutinho.cassiano@gmail.com>
2023-05-09 15:58:51 -0300

    feat(pages): add styles

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html lang="pt-BR">
       <Head />
-      <body className="font-primary max-w-lg mx-auto">
+      <body className="font-primary max-w-[414px] mx-auto bg-gray-100">
         <Main />
         <NextScript />
       </body>
