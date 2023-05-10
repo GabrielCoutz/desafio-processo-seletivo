abe603ad5c4299824f122c68c3c4d935e9460a6b
Gabriel Coutinho <gabriel.coutinho.cassiano@gmail.com>
2023-05-10 14:51:23 -0300

    feat(components/marketplace): hook implemented

--- src/components/Marketplace/index.tsx
+++ src/components/Marketplace/index.tsx
@@ -1,6 +1,10 @@
 import React from "react";
+import { useOrangeCards } from "../../hooks/useOrangeCards";
 
 export const Marketplace = () => {
+  const { activeCard, cards, handleChangeCard, handleClickCard } =
+    useOrangeCards();
+
   return (
     <section className="bg-white-100">
       <div className="px-4">
@@ -12,23 +16,34 @@ export const Marketplace = () => {
           por cada venda, sem letrinhas miúdas.
         </p>
         <div className="flex gap-3 mt-8 mb-2">
-          <span className="w-full h-1 bg-primary-400 rounded-lg"></span>
-          <span className="w-full h-1 bg-gray-100 rounded-lg"></span>
-          <span className="w-full h-1 bg-gray-100 rounded-lg"></span>
-          <span className="w-full h-1 bg-gray-100 rounded-lg"></span>
+          {cards.map((card) => (
+            <button
+              onClick={handleChangeCard}
+              className={`w-full h-1 rounded-lg ${
+                card.title === activeCard.title
+                  ? "bg-primary-400"
+                  : "bg-gray-100"
+              }`}
+              key={card.title}
+              data-value={card.title}
+            ></button>
+          ))}
         </div>
- 
-        <h2 className="text-md-3 font-semibold text-gray-800 text-center mb-5">
-          Pigz Marketplace
-        </h2>
       </div>
 
-      <div className="bg-gradient rounded-4xl h-96">
-        <div className="bg-phones bg-bottom h-full bg-no-repeat p-10">
-          <p className="text-white-100 font-light text-sm-11 text-center max-w-[302px] mx-auto">
-            Além de uma página exclusiva, com o Pigz Marketplace sua loja terá
-            mais visibilidade, no app e no site. Uma vitrine com milhares de
-            clientes todos os dias, pra vender muito mais.
+      <h2 className="text-md-3 font-semibold text-gray-800 text-center mb-5">
+        {activeCard.title}
+      </h2>
+
+      <div
+        className="bg-gradient rounded-4xl h-96 hover:cursor-pointer"
+        onClick={handleClickCard}
+      >
+        <div
+          className={`bg-bottom h-full bg-no-repeat p-10 bg-${activeCard.bg}`}
+        >
+          <p className="text-white-100 font-light text-sm-11 text-center max-w-[302px] mx-auto select-none">
+            {activeCard.text}
           </p>
         </div>
       </div>
