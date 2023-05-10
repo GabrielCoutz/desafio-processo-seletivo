fe9bc8077dd2b4c3978c8bad75a0dfb43d15e749
Gabriel Coutinho <gabriel.coutinho.cassiano@gmail.com>
2023-05-10 14:04:57 -0300

    feat(components/header): add blur on background

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 import { Button } from "../Button";
-import { Logo } from "../Icon/logo";
+import { Logo } from "../Icon/Logo";
 
 export const Header = () => {
   return (
-    <header className="flex justify-between p-4">
+    <header className="flex justify-between p-4 bg-white-100 bg-opacity-90 backdrop-blur-xl	 sticky top-0 z-10">
       <Logo />
       <Button.Primary gradient shadow className="text-sm-9">
         Já sou parceiro
