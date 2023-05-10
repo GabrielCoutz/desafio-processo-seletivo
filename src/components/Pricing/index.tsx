52b1ec2c3e3d2a2a1e29ec400cdaa1c166c34376
Gabriel Coutinho <gabriel.coutinho.cassiano@gmail.com>
2023-05-10 08:12:30 -0300

    feat(components/pricing): pricing section complete

---
+++ src/components/Pricing/index.tsx
@@ -0,0 +1,63 @@
+import React from "react";
+import { Button } from "../Button";
+import { Plan } from "../Plan";
+
+export const Pricing = () => {
+  return (
+    <section className="bg-white-100 pt-20 px-4">
+      <h1 className="text-xl-1 font-bold text-gray-800 text-center">
+        E mais: suporte <br /> que realmente <br /> funciona!
+      </h1>
+
+      <p className="text-base-2 text-gray-800 mx-auto text-center max-w-[286px] mt-5 mb-10">
+        Respostas automáticas e robôs? Aqui não. Nossa equipe está sempre
+        disponível pra ajudar você e seus clientes.
+      </p>
+
+      <ul className="flex flex-col gap-5">
+        <li>
+          <Plan.Wrapper>
+            <Plan.Title>Pigz</Plan.Title>
+            <Plan.Subtitle>Tudo o que você precisa</Plan.Subtitle>
+            <Plan.List
+              list={[
+                { id: 1, item: "Pigz Marketplace" },
+                { id: 2, item: "Página exclusiva" },
+                { id: 3, item: "Pigz Gestão Desktop e Mobile" },
+                { id: 4, item: "Gestão de entregadores" },
+                { id: 5, item: "Vias de impressão personalizáveis" },
+              ]}
+            />
+            <Plan.Price>R$199/mês</Plan.Price>
+            <Plan.Cta>
+              <Button.Primary fullWidth fullHeight gradient shadow>
+                Vender no Pigz agora
+              </Button.Primary>
+            </Plan.Cta>
+          </Plan.Wrapper>
+        </li>
+        <li>
+          <Plan.Wrapper>
+            <Plan.Title>Pagamento On-line</Plan.Title>
+            <Plan.Subtitle>Segurança e agilidade</Plan.Subtitle>
+            <Plan.List
+              list={[
+                { id: 6, item: "Aceite pix e Cartão de Crédito" },
+                { id: 7, item: "Antecipação Pix Automática" },
+                { id: 8, item: "Não depende de maquininha de cartão" },
+                { id: 9, item: "Segurança para receber pedidos" },
+                { id: 10, item: "Agilidade para entregar" },
+              ]}
+            />
+            <Plan.Price>2,99% por transação</Plan.Price>
+            <Plan.Cta>
+              <Button.Primary fullWidth fullHeight gradient shadow>
+                Saiba mais
+              </Button.Primary>
+            </Plan.Cta>
+          </Plan.Wrapper>
+        </li>
+      </ul>
+    </section>
+  );
+};
